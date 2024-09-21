67081befed33fc0a426da6189b01814b9abe6ed0
Maaz Bin Aamir <163261282+Maaz-BinAamir@users.noreply.github.com>
2024-09-21 19:05:49 +0500

    Delete src directory

--- src/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.loginpage, name='login'),
-    path('signup', views.signuppage, name= 'signup'),
-    path('logout', views.logoutview , name='logout')
-]
--- src/checkout/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('checkout', views.checkout, name='checkout'),
-    path('process_checkout', views.process_checkout, name='process_checkout')
-]
